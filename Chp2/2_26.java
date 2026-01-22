6d69b0ee72fb7f06c379b56086125b1c959f6094
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 11:08:53 +0900

    초기값 지정하면서 배열 생성하기
    Corrected array initialization syntax and fixed typo in variable name.

---
+++ Chp2/2_26.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int anArray1[] = {1, 2, 3, 4, 5};
+        int[] anArray2 = {1, 2, 3, 4, 5};
+        
+        int anArray3[][] = {{1, 2, 3}, {4, 5, 6}};
+        int[][] anArrary4 = {{1, 2, 3}, {4, 5, 6}};
+        
+        int anArray5[];
+        anArray5 = {1, 2, 3, 4, 5}; //오류, new int[]{1, 2, 3, 4, 5} 해야 함.
+        
+    }
+}
