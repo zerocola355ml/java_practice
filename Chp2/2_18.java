8db7b590ce08b946ef1e315a9c9389109eab558d
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 10:31:09 +0900

    향상된 for문의 예

---
+++ Chp2/2_18.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String args[]){
+		int[] nArray = {32, 87, 3, 589, 12, 1076, 2000, 8, 622, 127};
+		
+		for(int e : nArray){
+			System.out.print(e + " ");
+		}
+		System.out.println();
+	}
+}
