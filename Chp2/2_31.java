441cfe9cefb8a6dedb6ad76db381caea52983a7c
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 11:10:47 +0900

    +연산자의 사용

---
+++ Chp2/2_31.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String szStr1 = "Java ";
+        String szStr2 = "Application";
+        String szStr3 = szStr1 + szStr2;    //Java Application
+        szStr3 = "Java " + 2;               //Java 2
+        szStr3 = "Java " + 2.0;             //Java 2.0
+        szStr3 = true + " lies";            //true lies
+    }
+}
