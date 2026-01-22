2ea0ad9d96c30ca9092351b6a0ef7ed0949be2a3
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 10:18:53 +0900

    2_4 주석 달기의 예

---
+++ Chp2/2_4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+/**
+* File name: HelloApplication.java
+* Created by: Jung
+* Created on: March 2023
+* Modified on: June 2023
+*/
+
+class Main {
+/* main method
+    Display a simple string. */
+    public static void main(String[] args) {
+        System.out.println("Hello, Java Application!");
+    }
+}
