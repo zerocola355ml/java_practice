2c882c1b147ac6958bb4edcbb7bbf3b3bf27ff81
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 11:08:31 +0900

    배열 선언의 예

---
+++ Chp2/2_25.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int anArray1[];
+        int[] anArray2;
+        //int anArray3[10]; 오류, 크기를 명시할 수 없다. 
+        //anArrary1[0] = 1; 오류, 배열 초기화나 생성 전에는 사용할 수 없다. 
+        
+        int anArray4[][];
+        int[][] anArrary5;
+        int[] anArrary6[]; //오류는 아니지만 좋지 않은 표현이다. 
+        
+    }
+}
