76ee9414b3edf162a8aeb5a5b75b6be92a5aabb8
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 10:19:15 +0900

    식별자의 예

---
+++ Chp2/2_5.java
@@ -0,0 +1,7 @@
+public class HelloApplication{                         //클래스 이름
+	static String szMsg = "Hello, Java Application!";    //헝가리언 표기법
+	public static void main(String args[]){              //메소드 이름
+		int nTest = 0;                                     //변수 이름
+		System.out.println(szMsg);
+	}
+}
