24baba359208d65127c0e5f1a59737045ccaaf13
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 11:09:56 +0900

    문자열의 생성1

---
+++ Chp2/2_29.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String szStr1 = "Java Application";
+        String szStr2, szStr3;
+        szStr2 = "Java Apllet";
+        szStr3 = szStr2;
+    }
+}
