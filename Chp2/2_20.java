d66bcfc892ff5c5cb13af702737fb37dba75c1dd
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 10:31:48 +0900

    break문의 예1

---
+++ Chp2/2_20.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int nSum = 0;
+        int i = 1;
+        while (true){
+            nSum += i;
+            i++;
+            if (i > 10) break;
+        }
+    }
+}
