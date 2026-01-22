0cafd954ba26ee7650c6ae46bb2333a674822cec
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 10:31:27 +0900

    while문의 예

---
+++ Chp2/2_19.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int nSum = 0;
+        int i = 1;
+        while (i <= 10){
+            nSum += i;
+            i++;
+        }
+    }
+}
