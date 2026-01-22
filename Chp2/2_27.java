9040e6e9d2ab80f0a1d9c097a6baa32857c03092
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-22 11:09:17 +0900

    초기값 지정 없이 배열 생성하기
    Added multiple array declarations in Main class.

---
+++ Chp2/2_27.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int anArray1[] = new int[5];
+        int[] anArray2 = new int[5];
+        int anArray3[];
+        anArray3 = new int[5];
+        
+        int anArray4[][] = new int[3][2];
+        int[][] anArray5 = new int[3][2];
+    }
+}
