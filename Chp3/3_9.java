7947aabdc37ae813d2b86d2f43e310fe141a0e39
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-24 08:22:53 +0900

    객체의 생성

---
+++ Chp3/3_9.java
@@ -0,0 +1,11 @@
+class TV{
+	private int nSize = 0;
+}
+
+public class main{
+	public static void main(String[] args){
+		TV tv1;          //TV 클래스를 데이터 타입으로 하는 변수 선언
+		tv1 = new TV();    //TV 클래스를 이용한 객체 tv의 생성
+		//한 줄로 쓰면 TV tv1 = new TV();    
+	}
+}
