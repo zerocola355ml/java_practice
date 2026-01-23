ebdbb1a939752ef20f59628d6286d6234d794cf3
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-24 08:27:34 +0900

    메소드의 오버로딩

---
+++ Chp3/3_29.java
@@ -0,0 +1,4 @@
+System.out.println();             //매개변수 없음
+System.out.println("Test string");//문자열 매개변수
+System.out.println(241);          //정수형 매개변수
+System.out.println(34.5);         //double형 매개변수
