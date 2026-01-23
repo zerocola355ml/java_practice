0aef58cf33615a91909e3dcb9016274107223e76
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-24 08:24:20 +0900

    static 필드의 사용3

---
+++ Chp3/3_15.java
@@ -0,0 +1,15 @@
+class MyMessages{
+	public static String msg1 = "Hello";
+	public static String msg2 = "Java";
+	public static String msg3 = "Application";
+	public static String msg4 = "Applet";
+}
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println(MyMessages.msg1);
+		System.out.println(MyMessages.msg2);
+		System.out.println(MyMessages.msg3);
+		System.out.println(MyMessages.msg4);
+	}
+}
