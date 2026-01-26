eb1f02630c93f6f0b5bcb090210671ac239fd581
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-26 11:16:03 +0900

    다중 타입 매개변수

---
+++ Chp5/5_7.java
@@ -0,0 +1,17 @@
+interface Pair<K, V> {
+	public K getKey();
+	public V getValue();
+}
+
+class OrderedPair<K, V> implements Pair<K, V> {
+	private K key;
+	private V value;
+	
+	public OrderedPair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public K getKey() {return key;}
+	public V getValue() {return value;}
+}
