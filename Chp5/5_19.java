66e34b22d8a9c759fb46c3de75d4ff58df46d63b
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-26 11:19:35 +0900

    익명 구현 클래스로 객체 생성하기

---
+++ Chp5/5_19.java
@@ -0,0 +1,13 @@
+interface MyInterface {
+    public void method();
+}
+
+public class Main {
+  public static void main(String args[]) {
+      MyInterface sub = new MyInterface() {
+          public void method() {System.out.println("sub1");}
+      }; //; 누락 주의
+      
+      sub.method();
+  }
+}
