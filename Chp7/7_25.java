7d06b1ce282e9c03569ea8d72ea9cd2aa87318e4
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:56:40 +0900

    기본형을 문자열로 변환

---
+++ Chp7/7_25.java
@@ -0,0 +1,6 @@
+String str1 = Byte.toString((byte)3);
+String str2 = Short.toString((short)3);
+String str3 = Integer.toString(4);
+String str4 = Long.toString(5);
+String str5 = Float.toString((float)1.34);
+String str6 = Double.toString(1.234);
