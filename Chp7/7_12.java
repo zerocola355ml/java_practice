5efaf3f58bd4676485d1d956a088130a571df1b5
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:53:39 +0900

    문자열의 검색
    Implement a Java program to find all occurrences of '박찬호' in a given string, both from the start and the end.

---
+++ Chp7/7_12.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]) {
+        String szStr1 = "오늘 박찬호 선수의 피칭은 거의 완벽에 가까웠다."
+        + "홈 개막전이 벌어진 12일 선수 소개 때 관중들로부터 유일하게 "
+        + "야유를 받았던 박찬호가 이날 기립박수를 받으며 마운드를 내려갔다.";
+        
+        int nStrIndex = -1;
+        do {
+            nStrIndex = szStr1.indexOf("박찬호", nStrIndex + 1);
+            if (nStrIndex >= 0) System.out.println(nStrIndex + ", ");
+        } while (nStrIndex >= 0);
+        
+        nStrIndex = szStr1.length();
+        do {
+            nStrIndex = szStr1.lastIndexOf("박찬호", nStrIndex - 1);
+            if (nStrIndex >= 0) System.out.println(nStrIndex + ", ");
+        } while (nStrIndex >= 0);
+    }
+}
