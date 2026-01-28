13a25b2463dc4f1e53f08e4fd9631542d5d3c16b
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:52:53 +0900

    문자열의 생성2

---
+++ Chp7/7_9.java
@@ -0,0 +1,8 @@
+String szStr1 = "Java Application";
+String szStr2 = "Java Application";
+String szStr3 = newe String("Java Application");
+
+/*
+szStr1, szStr2은 같은 메모리 주소를 가리키고, szStr3은 그렇지 않다. 
+따라서 리터럴을 이용ㅎ나 초기화가 좋다. 
+*/
