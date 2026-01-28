7e4e9d6a699989676d4291d238ed46566a666e7b
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:54:13 +0900

    문자열의 변환

---
+++ Chp7/7_15.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+        String szStr1 = " His stuff was terrific today.  ";
+        System.out.println(szStr1.trim());
+        System.out.println(szStr1.toUpperCase());
+        System.out.println("HELP".toLowerCase());
+        System.out.println("Helf me".replace('f', 'p'));
+    }
+}
