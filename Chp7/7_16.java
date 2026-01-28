d9e0bdb2e343d6a05d58c5a469a51300b43665fb
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:54:26 +0900

    다른 자료형을 문자열형으로 변환

---
+++ Chp7/7_16.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(String.valueOf(123));    //123
+        System.out.println(String.valueOf(5 > 3));  //true
+        System.out.println(String.valueOf(3.0));    //3.0
+        System.out.println(String.valueOf('c'));    //c
+        int[] a = new int[5]; 
+        System.out.println(String.valueOf(a));      //[@7344699f
+    }
+}
