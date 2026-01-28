0b4e8f720f9c70792c3a0b3bcdb4ed6783b96596
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-28 10:56:28 +0900

    문자열을 기본형으로 변환

---
+++ Chp7/7_24.java
@@ -0,0 +1,6 @@
+byte b = Byte.parseByte("8");
+short s = Short.parseShort("12");
+int n = Integer.parseInt("123");
+long l = Long.parseLong("1234");
+float f = Float.parseFloat("12.3");
+double d = Double.parseDouble("1.23E2");
