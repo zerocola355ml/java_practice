dd1466c51c59ba32bf2ab167b8f5e65bd28a36e7
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-30 04:59:20 +0900

    기본 스트림을 보조 스트림으로 감싸기

---
+++ Chp8/8_12.java
@@ -0,0 +1,5 @@
+InputStream is = new FileInputStream("c:\\Java\\Main.Java");
+BufferedInputStream bis = new BufferedInputStream(is);
+
+OutputStream os = new FileOutputStream("c:\\Java\\FileTemp.Java");
+BufferedOutputStream bos = new BufferedOutputStream(os);
