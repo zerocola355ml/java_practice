e39c1d693816c4de115281789def458334389f68
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-30 05:00:29 +0900

    Create 8_18.java키보드로 입력하고 화면에 출력하기2

---
+++ Chp8/8_18.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String args[]) {
+      int i;
+      try {
+          while((i = System.in.read()) != '끝') { //제대로 작동하지 않는다. 
+              System.out.print((char) i);
+          }
+      } catch (Exception e) {System.out.println(e);}
+  }
+}
