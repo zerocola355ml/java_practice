4ce8cb79f1684e5945bb96f8453f6f79cbadf695
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-01-30 05:01:04 +0900

    키보드로 입력하고 화면에 출력하기2

--- Chp8/8_18.java
+++ Chp8/8_18.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String args[]) {
       int i;
       try {
-          while((i = System.in.read()) != '끝') { //제대로 작동하지 않는다. 
+          while((i = System.in.read()) != '끝') { //제대로 작동하지 않는다.
               System.out.print((char) i);
           }
       } catch (Exception e) {System.out.println(e);}
