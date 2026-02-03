2b071d8da3a94a1507a69a1ce23047a5bf3270d0
zerocola355ml <87372635+zerocola355ml@users.noreply.github.com>
2026-02-03 10:59:57 +0900

    배열을 이용한 데이터 관리 예제

---
+++ Chp10/10_1.java
@@ -0,0 +1,12 @@
+public class Source_10_1 {
+    public static void main(String[] args) throws Exception {
+        int[] a = new int[10];
+        for(int i = 0; i < 10; i++) {
+            a[i] = 0;
+        }
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
